e154edb72090fa88f14772c864efb1833f0f05c6
agustingorgni <agustingorgni25@gmail.com>
2023-08-27 10:28:09 -0300

    new style on list

--- src/pages/List/index.jsx
+++ src/pages/List/index.jsx
@@ -1,29 +1,29 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
 import styles from './styles.module.scss';
-import { MemorizedAvatar as Avatar } from "../../components/Avatar";
 
 export default function List() {
     const celebrities = useLoaderData();
     // TODO: ver de enviar un error para mostrarlo en una pagina general
     if (!celebrities || celebrities.length === 0) return <div>No stars were found 😞</div>;
-
+    console.log(celebrities);
     return (
         <section className={styles.list}>
             {
-                celebrities.map((celeb, i) => (
-                    <Link to={`/famouser/stars/${celeb.name.replace(' ', '-')}`} key={i}>
-                        <div className={styles.card}>
+                celebrities.map(
+                    celebrity => (
+                        <div key={celebrity.name} className={styles.card}>
                             <div className={styles.card__image}>
-                                <img src="/famouser/img/avatar.jpg" width="100%" />
+                                <img src="/famouser/img/avatar.jpg" width="100%" height="100%" alt={celebrity.name} loading="lazy" />
+                                <div className={styles.card__cta}>👁️</div>
                             </div>
-                            <hr className={styles.card__divider} />
-                            <div className={styles.card__information}>
-                                <span>{celeb.name}</span>
+                            <div className={styles.card__description}>
+                                <span>{celebrity.name}</span>
+                                <span>$ {celebrity.net_worth}</span>
                             </div>
                         </div>
-                    </Link>
-                ))
+                    )
+            )
             }
         </section>
     )
