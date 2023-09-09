af536fe581a200f9cdfe9774acc923b10537688f
agustingorgni <agustingorgni25@gmail.com>
2023-09-09 20:06:53 -0300

    feat(Snackbar): snackbar added and reducer actions modified

--- src/providers/FamouserProvider.jsx
+++ src/providers/FamouserProvider.jsx
@@ -1,24 +1,35 @@
 import { createContext, useReducer } from 'react';
 import PropTypes from 'prop-types';
+import { HIDE_SNACKBAR, SHOW_SNACKBAR } from '../utils/enums/actions';
 
 export const FamouserContext = createContext();
 
 export const FamouserProvider = ({ children }) => {
+    const initialState = {
+        snackbar: {
+            show: false,
+            text: null,
+            type: null
+        },
+    };
+
     const reducer = (state, action) => {
         const { type, payload } = action;
 
         switch (type) {
-            case 'CHANGE_HEADER_VISIBILITY': {
-                return { ...state, header: payload };
+            case SHOW_SNACKBAR: {
+                return { ...state, snackbar: { show: true, ...payload } };
+            }
+            case HIDE_SNACKBAR: {
+                return {
+                    ...state,
+                    snackbar: initialState.snackbar,
+                }
             }
             default: {
                 return state;
             }
         }
-    };
-
-    const initialState = {
-        header: true,
     };
 
     const [state, dispatch] = useReducer(reducer, initialState);
