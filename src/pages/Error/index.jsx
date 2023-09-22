48336f4ecb4313459bd7a17649d5b3c05d9aee13
agustingorgni <agustingorgni25@gmail.com>
2023-09-22 18:45:57 -0300

    styles(List): background added on Card

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -2,7 +2,9 @@ import { Link, useRouteError } from 'react-router-dom';
 import React from 'react';
 
 import styles from './styles.module.scss';
+
 import { INDEX } from '../../utils/enums/links';
+import { GENERIC_ERROR } from '../../utils/enums/messages';
 
 const Error = () => {
     const error = useRouteError();
@@ -10,10 +12,10 @@ const Error = () => {
     return (
         <section className={styles.error}>
             <div className={styles.error__image}>
-                <img src="/famouser/img/sad.jpg" width="100%" height="100%" alt="error" />
+                <img src="/famouser/img/sad.jpg" width="100%" height="300px" alt="error" />
             </div>
             <div className={styles.error__message}>
-                <span>{error?.message ?? 'Something wrong just happened'}</span>
+                <span>{error?.message ?? GENERIC_ERROR}</span>
             </div>
             <Link to={INDEX}>Back home</Link>
         </section>
